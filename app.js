82ab173c7024f0e0a4190aed427ff6f3f3e58811
monmatprogrammer <monsokmeanmon@gmail.com>
2025-03-01 08:15:27 +0700

    change

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
 const express = require("express");
 const morgan = require("morgan");
 const jwt = require("jsonwebtoken");
+const mySqlPool = require("./config/db");
+
+require("dotenv").config();
+
+const app = express();
+
+//Middleware
+app.use(express.json());
+app.use(morgan("dev"));
+
+//PORT
+const PORT = process.env.PORT || 5000;
+
+mySqlPool
+  .query("SELECT 1")
+  .then(() => {
+    console.log("Database connected");
+    app.listen(PORT, () => {
+      console.log(`PORT: ${PORT}`);
+      console.log("Server is running...");
+    });
+  })
+  .catch((error) => {
+    console.error(`Error occured: ${error}`);
+  });
